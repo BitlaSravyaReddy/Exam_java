eb8ec1526043ff25e772dc9a4ae6b8be7fa59261
Sravya Reddy <152065656+BitlaSravyaReddy@users.noreply.github.com>
2025-07-26 10:30:43 +0530

    Update Exam.java

--- Exam.java
+++ Exam.java
@@ -1,6 +1,6 @@
 class Exam{
 
-public static void main(System args[]){
+public static void main(String[] args){
 System.out.println("Name:Karthik");
 System.out.println("Rollno:251-22-737-010");
 System.out.println("Dept:IT");
