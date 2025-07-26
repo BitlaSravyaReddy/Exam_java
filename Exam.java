79409e55edd6cd71d5598443dcc421225ded5d18
Sravya Reddy <152065656+BitlaSravyaReddy@users.noreply.github.com>
2025-07-26 10:30:00 +0530

    Update Exam.java

--- Exam.java
+++ Exam.java
@@ -3,6 +3,6 @@ class Exam{
 public static void main(System args[]){
 System.out.println("Name:Karthik");
 System.out.println("Rollno:251-22-737-010");
-system.out.println("Dept:IT");
+System.out.println("Dept:IT");
 }
 }
